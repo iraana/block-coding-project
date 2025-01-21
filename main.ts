b551de768b82cb5752573929ba75ec83b77536d9
iraana <24048851@saultcollege.ca>
2025-01-21 11:06:18 -0800

    Testing

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+info.setScore(0)
+let mySprite2 = 0
+let mySprite = sprites.create(assets.image`myImage1`, SpriteKind.Player)
+animation.runMovementAnimation(
+mySprite,
+animation.animationPresets(animation.shake),
+2000,
+true
+)
+scene.setBackgroundImage(assets.image`jfgjfjg`)
