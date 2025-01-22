014f92d3b1523f1a1dc32085cb7ab63dcac4584c
Rafael Soares Cardoso <98930499+Kaos620@users.noreply.github.com>
2025-01-22 13:21:57 -0500

    Working on fruit logic

--- main.ts
+++ main.ts
@@ -10,13 +10,13 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setImage(assets.image`forestSnake4`)
 })
+let Fruit: Sprite = null
 let mySprite: Sprite = null
 info.setScore(0)
 mySprite = sprites.create(assets.image`forestSnake2`, SpriteKind.Player)
 controller.moveSprite(mySprite, 10, 10)
 scene.setBackgroundImage(assets.image`jfgjfjg`)
-game.onUpdateInterval(500, function () {
-    if (true) {
-    	
-    }
+game.onUpdateInterval(100, function () {
+    Fruit = sprites.create(assets.image`myImage`, SpriteKind.Food)
+    tiles.placeOnTile(Fruit, tiles.getTileLocation(randint(0, 100), randint(0, 10000)))
 })
