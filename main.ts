5dbe02b96e54f02a24e50d614c7e48223b98c641
Rafael Soares Cardoso <98930499+Kaos620@users.noreply.github.com>
2025-01-22 13:05:04 -0500

    Snake Heads Sprites

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`forestSnake1`)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`forestSnake2`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`forestSnake0`)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`forestSnake4`)
+})
+let mySprite: Sprite = null
 info.setScore(0)
-let mySprite2 = 0
-let mySprite = sprites.create(assets.image`forestSnake2`, SpriteKind.Player)
-animation.runMovementAnimation(
-mySprite,
-animation.animationPresets(animation.shake),
-2000,
-true
-)
+mySprite = sprites.create(assets.image`forestSnake2`, SpriteKind.Player)
+controller.moveSprite(mySprite, 10, 10)
 scene.setBackgroundImage(assets.image`jfgjfjg`)
+game.onUpdateInterval(500, function () {
+    if (true) {
+    	
+    }
+})
